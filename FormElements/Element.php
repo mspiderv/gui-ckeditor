b5222ba2b30228ff643ec27d44fbd33628e28bf0
Matej Vitaz <mspiderv@gmail.com>
2016-02-22 23:28:09 +0100

    commit

--- FormElements/Element.php
+++ FormElements/Element.php
@@ -3,8 +3,9 @@
 namespace Vitlabs\GUICKEditor\FormElements;
 
 use Vitlabs\GUICore\Traits\ElementTrait;
+use Vitlabs\GUICore\Contracts\Elements\ElementContract;
 
-abstract class Element {
+abstract class Element implements ElementContract {
 
     use ElementTrait;
 
